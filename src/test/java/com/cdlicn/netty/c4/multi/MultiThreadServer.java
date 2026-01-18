42a0b1c14b248a568031f3ae949c4f7774ce0de4
cdlicn <619356147@qq.com>
2026-01-18 20:09:31 +0800

    finish io part

--- src/test/java/com/cdlicn/netty/c4/multi/MultiThreadServer.java
+++ src/test/java/com/cdlicn/netty/c4/multi/MultiThreadServer.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.cdlicn.netty.c1.ByteBufferUtil.debugAll;
 
@@ -23,7 +24,12 @@ public class MultiThreadServer {
         bossKey.interestOps(SelectionKey.OP_ACCEPT);
         ssc.bind(new InetSocketAddress(8080));
         // 1. 创建固定数量的 worker 并初始化
-        Worker worker = new Worker("worker-0");
+        Worker[] workers = new Worker[Runtime.getRuntime().availableProcessors()]; // Runtime.getRuntime().availableProcessors() CPU可用的核心数
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new Worker("worker-" + i);
+        }
+//        Worker worker = new Worker("worker-0");
+        AtomicInteger index = new AtomicInteger();
         while (true) {
             boss.select();
             Iterator<SelectionKey> iter = boss.selectedKeys().iterator();
@@ -36,18 +42,78 @@ public class MultiThreadServer {
                     log.debug("connected...{}", sc.getRemoteAddress());
                     // 2. 关联 selector
                     log.debug("before register...{}", sc.getRemoteAddress());
-                    worker.register(sc); // boss调用，初始化 selector ， 启动 worker-0
+                    // round robin 轮询
+                    workers[index.getAndIncrement() % workers.length].register(sc); // boss调用，初始化 selector ， 启动 worker
+//                    worker.register(sc); // boss调用，初始化 selector ， 启动 worker-0
                     log.debug("after register...{}", sc.getRemoteAddress());
                 }
             }
         }
     }
 
+//    static class Worker implements Runnable {
+//        private Thread thread;
+//        private Selector selector;
+//        private String name;
+//        private ConcurrentLinkedQueue<Runnable> queue = new ConcurrentLinkedQueue<>();
+//        private volatile boolean start = false;
+//
+//        public Worker(String name) {
+//            this.name = name;
+//        }
+//
+//        // 初始化线程，和 selector
+//        public void register(SocketChannel sc) throws IOException {
+//            if (!start) {
+//                thread = new Thread(this, name);
+//                selector = Selector.open();
+//                thread.start();
+//                start = true;
+//            }
+//            // 向队列添加了任务，但这个任务并没有用立刻执行
+//            queue.add(() -> {
+//                try {
+//                    sc.register(selector, SelectionKey.OP_READ, null); // boss
+//                } catch (ClosedChannelException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            });
+//            selector.wakeup(); // 唤醒 select 方法
+//        }
+//
+//        @Override
+//        public void run() {
+//            while (true) {
+//                try {
+//                    selector.select(); // work-0 阻塞，wakeup
+//                    Runnable task = queue.poll();
+//                    if (task != null) {
+//                        task.run(); // 执行了 sc.register(selector, SelectionKey.OP_READ, null);
+//                    }
+//                    Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+//                    while (iter.hasNext()) {
+//                        SelectionKey key = iter.next();
+//                        iter.remove();
+//                        if (key.isReadable()) {
+//                            ByteBuffer buffer = ByteBuffer.allocate(16);
+//                            SocketChannel channel = (SocketChannel) key.channel();
+//                            log.debug("read...{}", channel.getRemoteAddress());
+//                            channel.read(buffer);
+//                            buffer.flip();
+//                            debugAll(buffer);
+//                        }
+//                    }
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }
+//    }
+
     static class Worker implements Runnable {
         private Thread thread;
         private Selector selector;
         private String name;
-        private ConcurrentLinkedQueue<Runnable> queue = new ConcurrentLinkedQueue<>();
         private volatile boolean start = false;
 
         public Worker(String name) {
@@ -62,15 +128,8 @@ public class MultiThreadServer {
                 thread.start();
                 start = true;
             }
-            // 向队列添加了任务，但这个任务并没有用立刻执行
-            queue.add(() -> {
-                try {
-                    sc.register(selector, SelectionKey.OP_READ, null); // boss
-                } catch (ClosedChannelException e) {
-                    throw new RuntimeException(e);
-                }
-            });
             selector.wakeup(); // 唤醒 select 方法
+            sc.register(selector, SelectionKey.OP_READ, null);
         }
 
         @Override
@@ -78,10 +137,6 @@ public class MultiThreadServer {
             while (true) {
                 try {
                     selector.select(); // work-0 阻塞，wakeup
-                    Runnable task = queue.poll();
-                    if (task != null) {
-                        task.run(); // 执行了 sc.register(selector, SelectionKey.OP_READ, null);
-                    }
                     Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
                     while (iter.hasNext()) {
                         SelectionKey key = iter.next();
