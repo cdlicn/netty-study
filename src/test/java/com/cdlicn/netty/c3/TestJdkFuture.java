a57cc9512d94022d771093e7f4adca60fd7fa198
cdlicn <619356147@qq.com>
2026-01-19 22:09:54 +0800

    future

---
+++ src/test/java/com/cdlicn/netty/c3/TestJdkFuture.java
@@ -0,0 +1,31 @@
+package com.cdlicn.netty.c3;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * @author cdlicn
+ * @date 2026年01月19日 21:58
+ * @description
+ */
+@Slf4j
+public class TestJdkFuture {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 1. 线程池
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        // 2. 提交任务
+        Future<Integer> future = service.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                log.debug("执行计算");
+                Thread.sleep(1000);
+                return 50;
+            }
+        });
+
+        // 3. 主线程通过 future 来获取结果
+        log.debug("等待结果");
+        log.debug("结果是{}", future.get());
+    }
+}
